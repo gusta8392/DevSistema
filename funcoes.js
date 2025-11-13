aa1fca915383bc6a62e65915ca5650545c2d7726
gusta8392 <202330037970@estudante.rn.gov.br>
2025-11-13 13:27:46 +0000

    login

---
+++ funcoes.js
@@ -0,0 +1,8 @@
+function fazerlogin(){
+    let login = document.getElementById('login').value
+    let senha = document.getElementById('senha').value
+
+    if(login === 'cenep' && senha === 'cenep123'){
+        window.location.href = 'pagina2.html'
+    }
+}
