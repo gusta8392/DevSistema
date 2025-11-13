1374cb697dca0ca0772815570aa1aff72d5783fa
gusta8392 <202330037970@estudante.rn.gov.br>
2025-11-13 13:41:56 +0000

    pagina2

--- funcoes.js
+++ funcoes.js
@@ -3,8 +3,14 @@ function fazerlogin(){
     let senha = document.getElementById('senha').value
 
     if(login === 'cenep' && senha === 'cenep123'){
-        window.location.href = 'pagina2.html'
+        alert('Login e Senha Corretos!'
+            localStorage.setItem('nome', login)
+            window.location.href = 'pagina2.html'
     }else{
         alert('login ou senha incorretos')
     }
+
+
     }
+ document.getElementById('titulo').innerHTML
+ localStorage.getItem('titulo')
